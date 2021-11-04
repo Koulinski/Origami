06d58b86278869ff90c68cbb2549f246234ef895
Koulinski <emil.k.koulinski@gmail.com>
2021-11-04 11:25:22 +0200

    Added Main

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import Menu from './components/Menu';
 import Header from './components/Header';
+import Main from './components/Main';
 import style from './App.module.css';
 
 function App() {
   return (
     <div className={style.app}>
       <Header />
+
+      <div className={style.container}>
+        <Menu />
+
+        <Main />
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Main/Main.js
@@ -0,0 +1,14 @@
+// import { Fragment } from 'react';
+
+import "./Main.css";
+
+const Main = () => {
+    return (
+        <main className="main-container">
+            <h1>Soooooooome Heading</h1>
+            <p>Some posts here</p>
+        </main>
+    );
+};
+
+export default Main;
